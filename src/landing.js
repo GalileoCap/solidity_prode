e572a3a0f6514003115de45d5fe0b6ef614b56d7
GalileoCap <galileocapp@gmail.com>
2021-09-19 23:01:40 -0300

    OK: Fonts, and mobile stacking

--- src/landing.js
+++ src/landing.js
@@ -6,42 +6,46 @@ import { useWeb3React } from '@web3-react/core'
 
 import { setPath } from './utils/urls.js'
 
-function ComoSeJuega() {
+function ComoSeJuega({ only }) {
 	//TODO: Font for the step numbers
 	return (
-		<Container>
+		<Grid.Column only={only} color='grey' width={7}>
 			<Header as='h1' inverted content='¿Cómo se juega?' />
 			<List as='ol'>
 				<List.Item as='li'>
 					Elegí una provincia
 				</List.Item>
+				<p />
 				<List.Item as='li'>
 					Ajustá los porcentajes para cada partido
 				</List.Item>
+				<p />
 				<List.Item as='li'>
 					Elegí cuántas fichas querés comprar
 				</List.Item>
 			</List>
 			<Button primary onClick={() => setPath('current')} style={{marginTop: '1em'}} content='Apostá ahora y ganá!' />
-		</Container>
+		</Grid.Column>
 	)
 }
 
 function Apostar() {
 	//TODO: Fix paths
 	return (
-		<Container style={{display: 'flex'}}>
-			<Image fluid src='../src/image.png' />
-			<Button color='red' style={{position: 'absolute', marginTop: '2em', marginLeft: '4em'}}onClick={() => setPath('current')} content='Apostar Ahora!' />
-		</Container>
+		<Grid.Column width={7} style={{display: 'flex', alignItems: 'center'}}> 
+			<Image fluid src='../resources/image.png' />
+			<Button color='red' style={{position: 'absolute'}} onClick={() => setPath('current')} content='Apostar Ahora!' />
+		</Grid.Column>
 	)
 }
 
 function VideoExplicativo() {
 	return (
-		<Container>
-			TODO: Video explicativo
-		</Container>
+		<Grid.Row columns={1} style={{marginTop: '1.3em'}}>
+			<Grid.Column color='green'>
+				TODO: Video explicativo
+			</Grid.Column>
+		</Grid.Row>
 	)
 }
 
@@ -51,18 +55,18 @@ export default function Landing() {
 	//TODO: Set pool font & size
 	return (
 		<Container style={{marginTop: '5em', textAlign: 'center'}}>
-			<Header style={{fontFamily: 'Ubuntu', fontSize: '2.5em', marginTop:'2.3em', marginBottom:'1em'}}>
+			<Header style={{fontFamily: 'Alpha Slab One', fontSize: '2.5em', marginTop:'2.3em', marginBottom: '0.8em'}}>
 				¡POZO ${totalPool}!
 			</Header>
-			<Grid rows={2} style={{marginLeft: '2em', marginRight: '2em'}}>
+			<Grid rows={2} stackable style={{marginLeft: '2em', marginRight: '2em'}}>
 				<Grid.Row columns={3}>
-					<Grid.Column color='grey' width={7} children={<ComoSeJuega />} />
-					<Grid.Column width={2} />
-					<Grid.Column width={7} children={<Apostar />}/>
+					<ComoSeJuega only='computer tablet' />
+					<Grid.Column only='computer tablet' width={2} />
+					<Apostar />
+					<Grid.Column only='mobile' width={2} />
+					<ComoSeJuega only='mobile' />
 				</Grid.Row>
-				<Grid.Row columns={1} style={{marginTop: '1.3em'}}>
-					<Grid.Column color='green' children={<VideoExplicativo />}/>
-				</Grid.Row>
+				<VideoExplicativo />
 			</Grid>
 		</Container>
 	)
